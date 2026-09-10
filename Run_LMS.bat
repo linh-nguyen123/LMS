4cb2e1d81c038633c9b5d233e7236ecdabad214f
Linh <vanlinhnguyen157@gmail.com>
2026-09-10 18:49:29 +0700

    chore: add 1-click launcher script Run_LMS.bat

---
+++ Run_LMS.bat
@@ -0,0 +1,4 @@
+@echo off
+set "PATH=D:\6.11.2\mingw_64\bin;D:\Tools\mingw1310_64\bin;D:\mingw64\bin;%PATH%"
+cd /d "%~dp0"
+start "" "build\Desktop_Qt_6_8_3_MinGW_64_bit-Debug\debug\LibraryManagementSystem.exe"
